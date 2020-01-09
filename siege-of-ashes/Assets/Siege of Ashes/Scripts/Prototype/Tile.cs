6d7d70f0077a49c8800a511edec437f7534ccfae
Christopher Robertson <christopherprobertson@gmail.com>
2020-01-09 13:55:06 +0000

    Added the the post processing package and incorporated it into the scene that I'm using for the movement testing named UnitMovement_SCN.unity.
    I started work on the movement prototype and created the Tile.cs struct along with the Unit.cs base parent class.
    
    In the unit class I've also produced a function a function that determines whether a unit can access tile depending on their movement points.
    
    **NOTE** This currently does not incorporate any pathfinding of sort and most likely will not until later phases of production where I'd recommend A* pathfinding. The mechanic itself still requires some extra content and therefore has not been tested yet.

---
+++ siege-of-ashes/Assets/Siege of Ashes/Scripts/Prototype/Tile.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SiegeOfAshes.Tiles
+{
+    [Serializable]
+    public struct Tile
+    {
+        public int X;
+        public int Y;
+        public bool IsPassable;
+    }
+}
---
+++ siege-of-ashes/Assets/Siege of Ashes/Scripts/Prototype/Unit.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using SiegeOfAshes.Tiles;
+
+namespace SiegeOfAshes.Movement
+{
+    public class Unit : MonoBehaviour
+    {
+        [Header("Attributes")]
+        [SerializeField]
+        private new string name;
+        [Space]
+
+        [Header("Movement Settings")]
+        [SerializeField]
+        private int movementPoints;
+
+        #region Tile Prediction
+        /// <summary>
+        /// A function that returns all of the tiles that the unit
+        /// can move to given the amount of movement points available.
+        /// </summary>
+        /// <param name="allTiles">
+        /// An array including all of the tiles in the scene.
+        /// </param>
+        /// <remarks>
+        /// **NOTE** This current does not incorporate any 
+        /// A* pathfinding yet, but will do during the stages after 
+        /// the prototype.
+        /// </remarks>
+        /// <returns></returns>
+        private Tile[] DetermineAvailableTiles(Tile[] allTiles)
+        {
+            Vector3 unitPosition = this.transform.position;
+            List<Tile> accessibleTiles = new List<Tile>();
+
+            foreach(Tile tile in allTiles)
+            {
+                if (tile.IsPassable)
+                {
+                    float tileDistance = Vector3.Distance(new Vector3(tile.X, 0, tile.Y), unitPosition);
+                    if (tileDistance < movementPoints)
+                    {
+                        accessibleTiles.Add(tile);
+                    }
+                }
+            }
+
+            return accessibleTiles.ToArray();
+        }
+        #endregion
+    }
+}
+
