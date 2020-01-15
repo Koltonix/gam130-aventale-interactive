5541daed2450b472e74267e1327d929b25f9d7f0
James Vanstone <jv228882@falmouth.ac.uk>
2020-01-15 11:52:58 +0000

    Add pause menu. Begin to add basic UI functionality.

---
+++ siege-of-ashes/Assets/Siege of Ashes/Scripts/UI/UIController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    private GameObject pauseMenu;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pauseMenu = GameObject.FindGameObjectWithTag("ui_pause_menu");
+        pauseMenu.SetActive(false);
+    }
+
+    private void togglePauseMenu()
+    {
+        if (pauseMenu.activeSelf)
+        {
+            pauseMenu.SetActive(false);
+            Time.timeScale = 1.0f;
+        }
+        else
+        {
+            pauseMenu.SetActive(true);
+            Time.timeScale = 0.0f;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            togglePauseMenu();
+        }
+    }
+}
---
+++ siege-of-ashes/Assets/Siege of Ashes/Scripts/UI/UIPauseMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIPauseMenu : MonoBehaviour
+{
+    private UIController uIController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        uIController = GameObject.FindObjectOfType<UIController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
