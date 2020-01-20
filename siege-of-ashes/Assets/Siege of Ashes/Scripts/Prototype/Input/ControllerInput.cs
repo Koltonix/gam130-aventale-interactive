27ebf0738617844132024a79eddd752f8c2ce3c1
Christopher Robertson <christopherprobertson@gmail.com>
2020-01-20 16:39:47 +0000

    Added some extra PlayerSettings Input to test the left joystick movement as well as the A button.
    Added some testing features to the ControllerInput.cs class to test if the PlayerSettings Input were working as intended.

--- siege-of-ashes/Assets/Siege of Ashes/Scripts/Prototype/Input/ControllerInput.cs
+++ siege-of-ashes/Assets/Siege of Ashes/Scripts/Prototype/Input/ControllerInput.cs
@@ -4,7 +4,14 @@ namespace SiegeOfAshes.Controls
 {
     public class ControllerInput : UserInput
     {
-        
+        public float horizontalJoystick;
+        public float aButton;
+
+        public override void Update()
+        {
+            horizontalJoystick = Input.GetAxis("JOYSTICK_AXIS_HORIZONTAL");
+            aButton = Input.GetAxis("JOYSTICK_BUTTON_A");
+        }
     }
 }
 
