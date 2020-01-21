403a806a5ff59a366c75edaa8747074502091e70
Christopher Robertson <christopherprobertson@gmail.com>
2020-01-21 11:21:56 +0000

    The controller now acts as input on moving an in game gizmo around the board in debugging; however, despite it working it doesn't provide the desired movement since it doesn't do it relative to the camera, so to fix this I'm going to move the icon around the screen space and then make a raycast from the camera instead in a very similar way to the keyboard.

--- siege-of-ashes/Assets/Siege of Ashes/Scripts/Prototype/Input/ControllerInput.cs
+++ siege-of-ashes/Assets/Siege of Ashes/Scripts/Prototype/Input/ControllerInput.cs
@@ -4,13 +4,63 @@ namespace SiegeOfAshes.Controls
 {
     public class ControllerInput : UserInput
     {
-        public float horizontalJoystick;
-        public float aButton;
+        [Header("Controller Axis")]
+        [SerializeField]
+        private float axisSensitivity;
+        [SerializeField]
+        private InputName horizontalAxis;
+        [SerializeField]
+        private InputName verticalAxis;
+
+        [Header("Controller Buttons")]
+        [SerializeField]
+        private InputName enterButton;
+
+        [Header("Ray Settings")]
+        private Vector3 cursorScreenPosition = new Vector3(0, 2.5f, 0);
 
         public override void Update()
         {
-            horizontalJoystick = Input.GetAxis("JOYSTICK_AXIS_HORIZONTAL");
-            aButton = Input.GetAxis("JOYSTICK_BUTTON_A");
+            AccessInputData();
+            MoveCursor();
+        }
+
+        public override bool HasClicked()
+        {
+            if (Input.GetButtonDown(enterButton.name)) return true;
+            return false;
+        }
+
+        public void MoveCursor()
+        {
+            cursorScreenPosition.x += horizontalAxis.value * axisSensitivity;
+            cursorScreenPosition.z += verticalAxis.value * axisSensitivity;
+        }
+
+        public override Ray GetRay()
+        {
+            return base.GetRay();
+        }
+
+        public override RaycastHit GetRaycastHit()
+        {
+            return base.GetRaycastHit();
+        }
+
+        private void AccessInputData()
+        {
+            horizontalAxis.value = Input.GetAxis(horizontalAxis.name);
+            verticalAxis.value = Input.GetAxis(verticalAxis.name);
+            enterButton.value = Input.GetAxis(enterButton.name);
+
+            HasClicked();
+        }
+
+        private void OnDrawGizmos()
+        {
+            //Gizmos.color = new Vector4(44, 199, 79, 128);
+            Gizmos.color = Color.green;
+            Gizmos.DrawSphere(cursorScreenPosition, .5f);
         }
     }
 }
