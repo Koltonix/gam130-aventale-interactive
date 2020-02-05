0d622214463816fec353de9ff633de8bacf9a819
Christopher Roberston <christopherprobertson@gmail.com>
2020-02-05 13:04:07 +0000

    - Started the Pathfinding by making a script named GenerateDebugBoard which quickly spawns a generic board that currently has no obstacles, but will do in the future just for testing.
    - Added an editor for the GenerateDebugBoard script for debugging purposes.

---
+++ cat-game/Assets/Cat Game/Scripts/Prototype/Pathfinding/GenerateDebugBoard.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace SiegeOfAshes.Pathfinding
+{
+    public class GenerateDebugBoard : MonoBehaviour
+    {
+        [Header("Board Settings")]
+        private Tile[,] board;
+        private List<Tile> debugBoard;
+
+        [SerializeField]
+        private int width;
+        [SerializeField]
+        private int height;
+        [SerializeField]
+        private Vector3 tileGap;
+
+        [SerializeField]
+        private Transform tileHolder;
+
+        [Header("Tile Settings")]
+        [SerializeField]
+        private GameObject tilePrefab;
+
+        public void GenerateBoard()
+        {
+            board = new Tile[width, height];
+            debugBoard = new List<Tile>();
+
+            for (int x = 0; x < width; x++)
+            {
+                for (int z = 0; z < height; z++)
+                {
+                    Vector3 spawnPosition = new Vector3(x * tileGap.x - ((width - 1) * tileGap.x * .5f),
+                                                        tileGap.y,
+                                                        z * tileGap.z - ((height - 1) * tileGap.z * .5f));
+
+                    GameObject worldTile = Instantiate(tilePrefab, spawnPosition, Quaternion.identity);
+                    worldTile.transform.parent = tileHolder;
+
+                    Tile tile = new Tile(worldTile, spawnPosition, false);
+                    board[x, z] = tile;
+                    debugBoard.Add(tile);
+                }
+            }
+        }
+
+        public void DeleteBoard()
+        {
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    DestroyImmediate(board[x, y].WorldReference);
+                }
+            }
+
+            board = new Tile[width, height];
+            debugBoard = new List<Tile>();
+        }
+    }
+}
+
