46c1f1b1e9c9756353252ed1f4c7a9fa15084fff
Christopher Roberston <christopherprobertson@gmail.com>
2020-02-05 13:04:07 +0000

    - Started the Pathfinding by making a script named GenerateDebugBoard which quickly spawns a generic board that currently has no obstacles, but will do in the future just for testing.
    - Added an editor for the GenerateDebugBoard script for debugging purposes.

---
+++ cat-game/Assets/Cat Game/Scripts/Prototype/Pathfinding/Tile.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+namespace SiegeOfAshes.Pathfinding
+{
+    [Serializable]
+    public class Tile : MonoBehaviour
+    {
+        public GameObject WorldReference;
+        public Vector2 Position;
+        public bool IsPassable;
+
+        public Tile(GameObject worldReference, Vector2 position, bool isPassable)
+        {
+            WorldReference = worldReference;
+            Position = position;
+            IsPassable = isPassable;
+        }
+    }
+}
+
