f44518cccebb99adaa872776087fc271d4701157
Christopher Robertson <christopherprobertson@gmail.com>
2020-02-07 11:05:33 +0000

    - Started the Pathfinding by creating an interface named IGetBoardData that transmits the board data from the GenerateDebugBoard to the PathfindingManager to adhere to adhere to the SOLID principles.

---
+++ cat-game/Assets/Cat Game/Scripts/Prototype/Pathfinding/Interfaces/IGetBoardData.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace SiegeOfAshes.Pathfinding
+{
+    public interface IGetBoardData
+    {
+        Tile[,] GetTiles();
+        int GetBoardWidth();
+        int GetBoardHeight();
+        Vector3 GetBoardCentre();
+    }
+}
---
+++ cat-game/Assets/Cat Game/Scripts/Prototype/Pathfinding/PathfindingManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using SiegeOfAshes.Data;
+
+namespace SiegeOfAshes.Pathfinding
+{
+    public class PathfindingManager : MonoBehaviour
+    {
+        #region Singleton
+        public static PathfindingManager Instance;
+        private void Awake()
+        {
+            if (Instance == null) Instance = this;
+            else Destroy(Instance);
+        }
+        #endregion
+
+        private IGetBoardData boardData;
+
+        private void Start()
+        {
+            boardData = GenerateDebugBoard.Instance.GetComponent<IGetBoardData>();
+        }
+
+        private void OnDrawGizmos()
+        {
+            if (boardData != null)
+            {
+                Gizmos.DrawWireCube(boardData.GetBoardCentre(), new Vector3(boardData.GetBoardWidth(), boardData.GetBoardCentre().y, boardData.GetBoardHeight()));
+            }
+            
+        }
+    }
+}
