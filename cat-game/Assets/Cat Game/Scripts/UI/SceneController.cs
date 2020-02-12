59a30a3bdc9d56bb6bb00f51a0c147697eabbfa5
Christopher Robertson <christopherprobertson@gmail.com>
2020-02-12 10:32:08 +0000

    - Added a SceneController class to be used on buttons while being generic enough.
    - *NOTE* I did intend for this to originally be static, but it would mean I can't derive from MonoBehaviour. This would mean that I couldn't place it on buttons due to how UnityEvents work unless I did it through code which at that point is more effort than it is worth.

---
+++ cat-game/Assets/Cat Game/Scripts/UI/SceneController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    public void LoadScene(int buildIndex)
+    {
+        SceneManager.LoadScene(buildIndex);
+    }
+
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
