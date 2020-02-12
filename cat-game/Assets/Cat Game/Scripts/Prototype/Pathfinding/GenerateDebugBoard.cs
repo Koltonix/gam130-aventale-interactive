9ac85b30ff13f837c8bd25a31fee2e0c2731134b
Christopher Robertson <christopherprobertson@gmail.com>
2020-02-12 22:04:19 +0000

    - Added the A* pathfinding algorithm to the PathfindingManager.
    - Added more signatures for the IGetBoardData which include the getting a tile based on a world position and neighbouring tiles using a tile.
    
    - Updated the GenerateDebugBoard too, but this will most likely be removed soon as my testing with it is coming to a close.
    
    - Added a boardX and boardY position for the tile to keep track of where it is in the board array.

--- cat-game/Assets/Cat Game/Scripts/Prototype/Pathfinding/GenerateDebugBoard.cs
+++ cat-game/Assets/Cat Game/Scripts/Prototype/Pathfinding/GenerateDebugBoard.cs
@@ -25,6 +25,7 @@ namespace SiegeOfAshes.Pathfinding
         private int height;
         [SerializeField]
         private Vector3 tileGap;
+        private Vector3 gridWorldSize;
 
         [SerializeField]
         private Transform tileHolder;
@@ -38,13 +39,15 @@ namespace SiegeOfAshes.Pathfinding
             board = new Tile[width, height];
             debugBoard = new List<Tile>();
 
+            gridWorldSize = new Vector3(width * tileGap.x, 0, height * tileGap.z);
+
             for (int x = 0; x < width; x++)
             {
-                for (int z = 0; z < height; z++)
+                for (int y = 0; y < height; y++)
                 {
                     Vector3 spawnPosition = new Vector3(x * tileGap.x - ((width - 1) * tileGap.x * .5f),
                                                         tileGap.y,
-                                                        z * tileGap.z - ((height - 1) * tileGap.z * .5f));
+                                                        y * tileGap.z - ((height - 1) * tileGap.z * .5f));
 
                     GameObject worldTile = Instantiate(tilePrefab, spawnPosition, Quaternion.identity);
                     worldTile.transform.parent = tileHolder;
@@ -52,8 +55,8 @@ namespace SiegeOfAshes.Pathfinding
                     bool isPassable = Random.Range(0, 2) == 1 ? true : false;
                     if (isPassable) worldTile.GetComponent<Renderer>().material.color *= .5f;
 
-                    Tile tile = new Tile(worldTile, spawnPosition, isPassable);
-                    board[x, z] = tile;
+                    Tile tile = new Tile(x, y, worldTile, spawnPosition, isPassable);
+                    board[x, y] = tile;
                     debugBoard.Add(tile);
                 }
             }
@@ -90,6 +93,75 @@ namespace SiegeOfAshes.Pathfinding
         {
             return this.transform.position;
         }
+
+        public Tile GetTileFromWorldPosition(Vector3 position)
+        {
+            float xPoint = ((position.x + gridWorldSize.x * .5f) / gridWorldSize.x);
+            float zPoint = ((position.z + gridWorldSize.z * .5f) / gridWorldSize.z);
+
+            xPoint = Mathf.Clamp01(xPoint);
+            zPoint = Mathf.Clamp01(zPoint);
+
+            int x = Mathf.RoundToInt((gridWorldSize.x - 1) * xPoint);
+            int y = Mathf.RoundToInt((gridWorldSize.y - 1) * zPoint);
+
+            return board[x, y];
+        }
+
+        public Tile[] GetNeighbouringTiles(Tile tile)
+        {
+            List<Tile> neighbouringTiles = new List<Tile>();
+
+            Tile checkingTile;
+            int xCheck;
+            int yCheck;
+
+            //Right hand check
+            xCheck = tile.boardX + 1;
+            yCheck = tile.boardY;
+
+            checkingTile = CheckForNeighbour(xCheck, yCheck);
+            if (checkingTile != null) neighbouringTiles.Add(checkingTile);
+
+
+            //Left hand check
+            xCheck = tile.boardX - 1;
+            yCheck = tile.boardY;
+
+            checkingTile = CheckForNeighbour(xCheck, yCheck);
+            if (checkingTile != null) neighbouringTiles.Add(checkingTile);
+
+            //Upper hand check
+            xCheck = tile.boardX;
+            yCheck = tile.boardY + 1;
+
+            checkingTile = CheckForNeighbour(xCheck, yCheck);
+            if (checkingTile != null) neighbouringTiles.Add(checkingTile);
+
+            //Left hand check
+            xCheck = tile.boardX;
+            yCheck = tile.boardY - 1;
+
+            checkingTile = CheckForNeighbour(xCheck, yCheck);
+            if (checkingTile != null) neighbouringTiles.Add(checkingTile);
+
+            return neighbouringTiles.ToArray();
+
+        }
+
+        private Tile CheckForNeighbour(int xCheck, int yCheck)
+        {
+            if (xCheck >= 0 && xCheck < width)
+            {
+                if (yCheck >= 0 && yCheck < height)
+                {
+                    return board[xCheck, yCheck];
+                }
+            }
+
+            return null;
+        }
+
         #endregion
     }
 }
