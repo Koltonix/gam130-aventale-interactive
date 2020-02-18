7b08c93df3365f13cf480bc6c21ca311557f44e7
James Vanstone <JV228882@falmouth.ac.uk>
2020-02-18 17:27:21 +0000

    Add WorldSpaceMenu script to turn something towards main camera at all times. Add a health bar prefab and add it to the health class. Health bar is spawned on unit creation, and shows up when the mouse is over the collider of the unit.

--- cat-game/Assets/Cat Game/Scripts/Prototype/Unit/Health.cs
+++ cat-game/Assets/Cat Game/Scripts/Prototype/Unit/Health.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 namespace CatGame.Units
 {
     public class Health : MonoBehaviour
@@ -9,7 +9,36 @@ namespace CatGame.Units
         [SerializeField]
         private int maxHealth;
         private int currentHealth;
+
+        [SerializeField]
+        private GameObject healthBarPrefab;
+        private GameObject healthBar;
+        private Image healthBarImage;
+
+        [SerializeField]
+        private Animator animator;
+
+
+        void Start()
+        {
+            currentHealth = maxHealth;
+            healthBar = Instantiate(healthBarPrefab, this.transform);
+            healthBar.transform.position += new Vector3(0,1.5f,0);
+            healthBar.SetActive(false);
+            healthBarImage = healthBar.GetComponentInChildren<Image>();
+        }
         
+        void OnMouseEnter()
+        {
+            healthBarImage.fillAmount = currentHealth / maxHealth;
+            healthBar.SetActive(true);
+        }
+
+        void OnMouseExit()
+        {
+            healthBar.SetActive(false);
+        }
+
         public int GetHealth()
         {
             return currentHealth;
@@ -18,6 +47,23 @@ namespace CatGame.Units
         public void Heal(int heal)
         {
             currentHealth = Mathf.Clamp(currentHealth + heal, 0, maxHealth);
+        }
+
+        public void Damage(int damage)
+        {
+            currentHealth = Mathf.Clamp(currentHealth + damage, 0, maxHealth);
+            if (currentHealth == 0)
+            {
+                //if (animator != null)
+                //{
+                    //animator.Play();
+                //}
+                Delete();
+            }
+        }
+        public void Delete()
+        {
+            Destroy(gameObject);
         }
     }
 }
---
+++ cat-game/Assets/Cat Game/Scripts/UI/WorldSpaceMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CatGame.UI
+{
+    public class WorldSpaceMenu : MonoBehaviour
+    {
+        Camera worldCamera;
+        // Start is called before the first frame update
+        void Start()
+        {
+            worldCamera = Camera.main;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            gameObject.transform.rotation = worldCamera.transform.rotation;
+        }
+    }
+}
