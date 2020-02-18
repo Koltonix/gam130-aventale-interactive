30cb78fbe173311523efc135a9f796bc1f120d4d
James Vanstone <JV228882@falmouth.ac.uk>
2020-02-18 16:27:45 +0000

    Add Health.cs for development.

---
+++ cat-game/Assets/Cat Game/Scripts/Prototype/Unit/Health.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CatGame.Units
+{
+    public class Health : MonoBehaviour
+    {
+        [SerializeField]
+        private int maxHealth;
+        private int currentHealth;
+        
+        public int GetHealth()
+        {
+            return currentHealth;
+        }
+
+        public void Heal(int heal)
+        {
+            currentHealth = Mathf.Clamp(currentHealth + heal, 0, maxHealth);
+        }
+    }
+}
