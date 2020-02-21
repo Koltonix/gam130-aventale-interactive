05dea76f2d8d56dd451110aa8eae15807dea2263
James Vanstone <JV228882@falmouth.ac.uk>
2020-02-21 11:56:02 +0000

    Adding combat namespace and attacker class.

---
+++ cat-game/Assets/Cat Game/Scripts/UI/Combat/Attacker.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CatGame.Units;
+
+namespace CatGame.Combat
+{
+    public class Attacker : MonoBehaviour
+    {
+        public int Damage;
+    }
+}
