512c270af76ffe7fce12ef2ad680cc9b4774a1cd
James Vanstone <JV228882@falmouth.ac.uk>
2020-02-25 14:38:40 +0000

    Did a bit of research into the unity editor scripting for custom tools.

--- cat-game/Assets/Cat Game/Scripts/UI/Combat/Attacker.cs
+++ cat-game/Assets/Cat Game/Scripts/UI/Combat/Attacker.cs
@@ -2,11 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 using CatGame.Units;
+using CatGame.Controls;
 
 namespace CatGame.Combat
 {
     public class Attacker : MonoBehaviour
     {
         public int Damage;
-    }
+        //public GameObject ArrowPrefab;
+        [SerializeField]
+        private GameObject arrow;
+        private bool active;
+
+        void Start()
+        {
+            //arrow = Instantiate(ArrowPrefab);
+            active = false;
+            arrow.SetActive(active);
+        }
+
+        void Update()
+        {
+            if (active)
+            {
+                
+            }
+        }
+    /// <summary>
+    /// void OnMouseClick()
+    ///{
+    ///active = !active;
+    ///arrow.SetActive(active);
+    ///}
+    /// </summary>
+
+}
 }
