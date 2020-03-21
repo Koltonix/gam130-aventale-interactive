0b2cba7d1f4016c22f1da0663d8125d82e5a385f
Christopher Robertson <christopherprobertson@gmail.com>
2020-03-21 12:24:06 +0000

    - Created a Camera Rotator script which moves the camera to the next position in an array of camera points. It also rotates the camera too.
    - Created a CameraPoint struct to store the world position, rotation, and if it should use the object's rotation, or the world rotation on the GameObject.

---
+++ cat-game/Assets/Cat Game/Scripts/Gameplay/CameraPoint.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+namespace CatGame.CameraMovement
+{
+    [Ser]
+    public struct CameraPoint
+    {
+        public Transform worldTransform;
+        public Vector3 rotation;
+        public bool useObjectRotation;
+    }
+}
---
+++ cat-game/Assets/Cat Game/Scripts/Gameplay/CameraRotator.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace CatGame.CameraMovement
+{
+    public class CameraRotator : MonoBehaviour
+    {
+        [Header("Input Settings")]
+        [SerializeField]
+        private KeyCode positiveButton = KeyCode.E;
+        [SerializeField]
+        private KeyCode negativeButton = KeyCode.Q;
+        [Space]
+
+        [Header("Camera Attributes")]
+        [SerializeField]
+        private Transform cameraTransform;
+        [SerializeField]
+        private CameraPoint[] travelPoints;
+        [SerializeField]
+        private float moveSpeed = 1.25f;
+        [SerializeField]
+        private float rotateSpeed = 1.25f;
+        private int pointIndex = 0;
+        [Space]
+
+        [Header("Coroutines")]
+        private Coroutine movementCoroutine;
+        private Coroutine rotationCoroutine;
+
+        private void Start()
+        {
+            if (!cameraTransform) cameraTransform = this.transform;
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(positiveButton)) IterateThroughPoints(1);
+            else if (Input.GetKeyDown(negativeButton)) IterateThroughPoints(-1);
+        }
+
+        private void IterateThroughPoints(int direction)
+        {
+            //Sanity check to ensure it is either 1 or -1
+            int absDirection = Mathf.Abs(direction);
+            direction = direction / absDirection;
+
+            pointIndex += direction;
+            if (pointIndex > travelPoints.Length - 1) pointIndex = 0;
+            else if (pointIndex < 0) pointIndex = travelPoints.Length - 1;
+
+            UpdateCamera(travelPoints[pointIndex]);
+        }
+
+        private void UpdateCamera(CameraPoint point)
+        {
+            if (movementCoroutine != null) StopCoroutine(movementCoroutine);
+            if (rotationCoroutine != null) StopCoroutine(rotationCoroutine);
+
+            movementCoroutine = StartCoroutine(MoveToPoint(point, moveSpeed));
+            rotationCoroutine = StartCoroutine(RotateToPoint(point, rotateSpeed));
+        }
+
+        private IEnumerator MoveToPoint(CameraPoint point, float moveSpeed)
+        {
+            Vector3 targetPoint = point.worldTransform.position;
+
+            float t = 0.0f;
+            while (t <= 1.0f)
+            {
+                t += Time.deltaTime * moveSpeed;
+                cameraTransform.position = Vector3.Lerp(cameraTransform.position, targetPoint, t);
+
+                yield return new WaitForEndOfFrame();
+            }
+
+            yield return null;
+        }
+
+        private IEnumerator RotateToPoint(CameraPoint point, float rotateSpeed)
+        {
+            Quaternion targetRotation = GetRotationFromCameraPoint(point);
+
+            float t = 0.0f;
+            while (t <= 1.0f)
+            {
+                t += Time.deltaTime * rotateSpeed;
+                cameraTransform.rotation = Quaternion.Lerp(cameraTransform.rotation, targetRotation, t);
+
+                yield return new WaitForEndOfFrame();
+            }
+
+            yield return null;
+        }
+
+        private Quaternion GetRotationFromCameraPoint(CameraPoint point)
+        {
+            if (point.useObjectRotation) return point.worldTransform.rotation;
+            return Quaternion.Euler(point.rotation);
+        }
+    }
+}
