afee416ab957bcf0c72674f59846089a75ba2a1f
Christopher Robertson <christopherprobertson@gmail.com>
2020-04-07 13:44:23 +0100

    - Added a static particle effect manager that can be called at any time and will delete the particle once its lifetime has ended.

---
+++ cat-game/Assets/Cat Game/Scripts/Particle Effects/Particle Manager.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public static class ParticleManager
+{
+    public static void SpawnParticle(GameObject particle, Vector3 position, Quaternion rotation)
+    {
+        ParticleSystem particleClone = GameObject.Instantiate(particle, position, rotation).GetComponent<ParticleSystem>();
+        GameObject.Destroy(particleClone.gameObject, particleClone.main.startLifetime.constant + particleClone.main.duration);
+    }
+}
