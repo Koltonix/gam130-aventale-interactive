4d3e1ad62dded77b1c1d60876eedde924387f440
Christopher Robertson <christopherprobertson@gmail.com>
2020-04-15 22:56:22 +0100

    - Added an editor tool that numbers prefabs with a custom suffix when added to a scene so as long as the prefab has been assigned.
    - Made it for a friend for his project, so I thought I'd include it just in case we ever need it.

---
+++ cat-game/Assets/Cat Game/Scripts/Prefab Renaming/PrefabNumbering.cs
@@ -0,0 +1,59 @@
+﻿using System.Linq;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class PrefabNumbering : MonoBehaviour
+{
+    public GameObject prefab;
+    public string extraText = "#";
+
+    private GameObject[] allPrefabs;
+    private GameObject[] previousPrefabs;
+
+    private int worldCount = 0;
+    private int previousWorldCount = 0;
+
+    private void Update()
+    {
+        if (prefab)
+        {
+            worldCount = FindObjectsOfType<GameObject>().Length;
+
+            if (previousWorldCount != worldCount)
+            {
+                CheckNewItem();
+            }
+        } 
+    }
+
+    private void CheckNewItem()
+    {
+        allPrefabs = GameObject.FindObjectsOfType<GameObject>().Where(obj => UnityEditor.PrefabUtility.GetPrefabParent(obj) == prefab).ToArray();
+        if (allPrefabs.Length > 0 && previousPrefabs != null)
+        {
+            for (int i = 0; i < previousPrefabs.Length; i++)
+            {
+                //Already existed
+                if (allPrefabs[0] == previousPrefabs[i])
+                {
+                    return;
+                }
+            }
+
+            allPrefabs[0].name += extraText + allPrefabs.Length;
+        }
+
+        previousPrefabs = allPrefabs;
+    }
+
+    public void RenameAllGameObjects()
+    {
+        GameObject[] allObjects = allPrefabs = GameObject.FindObjectsOfType<GameObject>().Where(obj => UnityEditor.PrefabUtility.GetPrefabParent(obj) == prefab).ToArray();
+
+        //Reversed as the 0th element is the latest instance
+        for (int i = allObjects.Length - 1; i >= 0; i--)
+        {
+            allPrefabs[i].name = prefab.name + extraText + (allObjects.Length - i - 1);
+        }
+    }
+}
---
+++ cat-game/Assets/Cat Game/Scripts/Prefab Renaming/PrefabNumberingEditor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(PrefabNumbering))]
+public class PrefabNumberingEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        PrefabNumbering numbering = (PrefabNumbering)target;
+
+        if (GUILayout.Button("Rename all Prefabs"))
+        {
+            numbering.RenameAllGameObjects();
+        }
+    }
+}
