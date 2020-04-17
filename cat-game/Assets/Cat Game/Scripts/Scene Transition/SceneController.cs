b2a99bd95b8cff44246182ae67e6735c6c1654eb
Christopher Robertson <christopherprobertson@gmail.com>
2020-04-17 13:56:46 +0100

    - Added comments and docstring to the SceneController and PauseMenu script.

--- cat-game/Assets/Cat Game/Scripts/Scene Transition/SceneController.cs
+++ cat-game/Assets/Cat Game/Scripts/Scene Transition/SceneController.cs
@@ -1,6 +1,9 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
+/// <summary>
+/// Handles Scene Transitioning for scripts and UnityEvents.
+/// </summary>
 public class SceneController : MonoBehaviour
 {
     #region Singleton Reference
@@ -12,21 +15,27 @@ public class SceneController : MonoBehaviour
     }
     #endregion
 
+    /// <summary>Loads the scene with a build index.</summary>
+    /// <param name="buildIndex">Scene Build Index.</param>
     public void LoadScene(int buildIndex)
     {
         SceneManager.LoadScene(buildIndex);
     }
 
+    /// <summary>Loads the scene with a string</summary>
+    /// <param name="sceneName">Scene Name.</param>
     public void LoadScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
     }
 
+    /// <summary>Reloads the current scene.</summary>
     public void RestartScene()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 
+    /// <summary>Quits the Application.</summary>
     public void QuitApplication()
     {
         Application.Quit();
