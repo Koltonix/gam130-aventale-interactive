1e0f2613d991de35f77c1c75e69ab1548a0bf9ba
Christopher Robertson <christopherprobertson@gmail.com>
2020-04-17 13:16:25 +0100

    - Added comments and docstring to all of the scripts in the Interfaces folder..

--- cat-game/Assets/Cat Game/Scripts/Interfaces/IGetBoardData.cs
+++ cat-game/Assets/Cat Game/Scripts/Interfaces/IGetBoardData.cs
@@ -3,6 +3,10 @@ using CatGame.Tiles;
 
 namespace CatGame.Pathfinding
 {
+    /// <summary>
+    /// Used to get all of the Tile Data and also to convert data
+    /// relevant to the tile and board.
+    /// </summary>
     public interface IGetBoardData
     {
         Tile[,] GetTiles();
--- cat-game/Assets/Cat Game/Scripts/Interfaces/IGetOnClick.cs
+++ cat-game/Assets/Cat Game/Scripts/Interfaces/IGetOnClick.cs
@@ -2,6 +2,7 @@
 
 namespace CatGame.Controls
 {
+    /// <summary>Used to get all of the relevant Input Data.</summary>
     public interface IGetOnClick
     {
         RaycastHit GetRaycastHit();
