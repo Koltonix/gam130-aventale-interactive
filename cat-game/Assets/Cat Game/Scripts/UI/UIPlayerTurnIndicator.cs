f909bc8c8caa009c51451af0e6eb35940cd9c22f
JamesV <jvanstone.work@gmail.com>
2020-04-18 01:08:06 +0100

    Add player turn indicator to the game ui scene.

---
+++ cat-game/Assets/Cat Game/Scripts/UI/UIPlayerTurnIndicator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CatGame.Data;
+using TMPro;
+
+namespace CatGame.UI
+{
+    public class UIPlayerTurnIndicator : MonoBehaviour
+    {
+        TextMeshProUGUI currentPlayerDisplay;
+        private void Start()
+        {
+            currentPlayerDisplay = gameObject.GetComponent<TextMeshProUGUI>();
+            TurnManager.Instance.onPlayerCycle += TurnEnd;
+        }
+
+        public void TurnEnd(Player currentPlayer)
+        {
+            currentPlayerDisplay.text = ("It's Player " + (currentPlayer.number + 1).ToString() + "'s turn");
+        }
+    }
+}
