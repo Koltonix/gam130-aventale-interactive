fed87d5d683f6a92aea960cc487133405cf93789
Christopher Robertson <christopherprobertson@gmail.com>
2020-04-30 14:11:04 +0100

    - Added a script that rotates the Skybox continuously to make the scene feel less static.
    - Started to set up the main menu by adding the board and buildings as well as changing the materials.

---
+++ cat-game/Assets/Cat Game/Scripts/Aesthetic/SkyboxRotator.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+
+/// <summary>Rotates the Skybox.</summary>
+/// <remarks>
+/// Sources: 
+/// https://www.youtube.com/watch?v=cqGq__JjhMM 
+/// https://forum.unity.com/threads/rotate-a-skybox.130639/
+/// </remarks>
+public class SkyboxRotator : MonoBehaviour
+{
+    public float rotateSpeed = 1.25f;
+
+    private float currentRotation;
+    public float CurrentRotation
+    { 
+        get { return currentRotation; }
+        set
+        {
+            currentRotation = value;
+            if (currentRotation > 360) currentRotation = 0;
+            else if (currentRotation < 0) currentRotation = 360;
+        }
+    }
+
+    private void Start()
+    {
+        currentRotation = RenderSettings.skybox.GetFloat("_Rotation");
+    }
+
+    private void Update()
+    {
+        CurrentRotation += rotateSpeed;
+        RenderSettings.skybox.SetFloat("_Rotation", CurrentRotation);
+    }
+}
